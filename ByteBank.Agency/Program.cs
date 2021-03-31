76a02693adadec4a15243672e368f72403a64f04
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-31 17:20:35 -0300

    Testing Class of List

--- ByteBank.Agency/Program.cs
+++ ByteBank.Agency/Program.cs
@@ -19,113 +19,36 @@ namespace ByteBank.Agency
         {
             #region Test
             Client client1 = new Client(name: "João", occupation: "Developer", cpf: "225846589-54");
-            Account account1 = new Account(client1, agencyNumber: 123, accountNumber: 126456, balance: 1000);
-
+            Client client2 = new Client(name: "José", occupation: "Developer", cpf: "225846589-53");
+            Client client3 = new Client(name: "Maria", occupation: "Developer", cpf: "225846589-52");
+            Client client4 = new Client(name: "Léo", occupation: "Developer", cpf: "225846589-51");
+            Client client5 = new Client(name: "Bruce", occupation: "Developer", cpf: "225846589-50");
+            Account account1 = new Account(client1, agencyNumber: 123, accountNumber: 456789, balance: 1000);
+            Account account2 = new Account(client2, agencyNumber: 123, accountNumber: 126456, balance: 1500);
+            Account account3 = new Account(client3, agencyNumber: 123, accountNumber: 123789, balance: 2000);
+            Account account4 = new Account(client4, agencyNumber: 123, accountNumber: 456123, balance: 500);
+            Account account5 = new Account(client5, agencyNumber: 123, accountNumber: 789123, balance: 1750);
+           
             Auxiliary auxiliary1 = new Auxiliary("José", cpf: "224159753-64");
 
             Director director1 = new Director(name: "Sthevan", cpf: "229852159-74");
 
             #endregion
 
-            #region Test Extract value
-            //string url = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
+            AccountList list = new AccountList();
 
-            //ExtractValueArgumentsURL extractValues = new ExtractValueArgumentsURL(url);
+            list.AddAccount(account1);
+            list.AddAccount(account2);
+            list.AddAccount(account3);
+            list.AddAccount(account4);
+            list.AddAccount(account5);
+            list.AddAccount(account5);
+            list.AddAccount(account5);
 
-            //string term1 = "moedaOrigem";
-
-            //string extracted = extractValues.GetValue(term1);
-
-            //Console.WriteLine("Moeda de origem: " + extracted);
-
-            //string term2 = "moedaDestino";
-            //string extracted2 = extractValues.GetValue(term2);
-            //Console.WriteLine("Moeda de destino: " + extracted2);
-            #endregion
-
-            #region StartsWith EndsWith Contains
-
-            //string urlTest = "http://www.bytebank.com/cambio";
-            //int indiceByteBank = urlTest.IndexOf("http://www.bytebank.com");
-
-            //Console.WriteLine(indiceByteBank == 0);
-
-            //Console.WriteLine(urlTest.StartsWith("http://www.bytebank.com"));
-            //Console.WriteLine(urlTest.EndsWith("cambio"));
-            #endregion
-
-            #region Patterns
-
-            //            string padrao =
-            //"[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
-
-            //            string padrao2 = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
-            //            string padrao3 = "[0-9]{4}[-][0-9]{4}";
-            //            string padrao4 = "[0-9]{4,5}[-][0-9]{4,5}";
-            //            string padrao5 = "[0-9]{4,5}[-]{0,1}[0-9]{4,5}";
-            //            string padrao6 = "[0-9]{4,5}-{0,1}[0-9]{4,5}";
-            //            string padrao7 = "[0-9]{4,5}-?[0-9]{4,5}";
-
-            //            string textoDeTeste = "Meu nome é Guilherme, me ligue em 94784-4546";
-            //            string textoDeTeste2 = "Meu nome é Guilherme, me ligue em 947844546";
-
-            //            Match resultado = Regex.Match(textoDeTeste, padrao);
-            //            Match resultado2 = Regex.Match(textoDeTeste, padrao2);
-            //            Match resultado3 = Regex.Match(textoDeTeste, padrao3);
-            //            Match resultado4 = Regex.Match(textoDeTeste, padrao4);
-            //            Match resultado5 = Regex.Match(textoDeTeste2, padrao5);
-            //            Match resultado6 = Regex.Match(textoDeTeste, padrao6);
-            //            Match resultado7 = Regex.Match(textoDeTeste2, padrao6);
-            //            Match resultado8 = Regex.Match(textoDeTeste, padrao7);
-            //            Match resultado9 = Regex.Match(textoDeTeste2, padrao7);
-
-            //            Console.WriteLine(resultado.Value);
-            //            Console.WriteLine(resultado2.Value);
-            //            Console.WriteLine(resultado3.Value);
-            //            Console.WriteLine(resultado4.Value);
-            //            Console.WriteLine(resultado5.Value);
-            //            Console.WriteLine(resultado6.Value);
-            //            Console.WriteLine(resultado7.Value);
-            //            Console.WriteLine(resultado8.Value);
-            //            Console.WriteLine(resultado9.Value);
-
-
-            #endregion
-
-            #region Class object
-
-            //object conta = new Account(client1, agencyNumber: 123, accountNumber: 126456, balance: 1000);
-            //Console.WriteLine(conta);
-
-
-            #endregion
-
-            #region Equals
-
-            Client client2 = null; /*new Client(name: "João", occupation: "Developer", cpf: "225846589-54");*/
-
-
-            Console.WriteLine(client1 == client2);
-            try
+            foreach (var item in list.ReturnList())
             {
-                if (client1.Equals(client2))
-                {
-                    Console.WriteLine("Equals");
-                }
-                else
-                {
-                    Console.WriteLine("Different");
-                }
+                Console.WriteLine($"Name: {item.Holder.Name} - Agency number: {item.AgencyNumber} - Account number: {item.AccountNumber} - Balance: {item.GetBalance():C}");
             }
-            catch (Exception ex)
-            {
-
-                Console.WriteLine(ex.Message);
-            }
-
-
-            #endregion
-
 
             Console.ReadKey();
         }
