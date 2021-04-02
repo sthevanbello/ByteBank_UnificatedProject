97a7433d32d7025e85df692df068d1e246be1c94
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-04-02 17:10:46 -0300

    Reading file 'contas.csv' using StreamReader and input in the List<Account>

--- ByteBank.Agency/Program.cs
+++ ByteBank.Agency/Program.cs
@@ -12,6 +12,7 @@ using Humanizer;
 using System.Text.RegularExpressions;
 using ByteBank.Agency.Extensions;
 using System.IO;
+using System.Globalization;
 
 namespace ByteBank.Agency
 {
@@ -19,44 +20,11 @@ namespace ByteBank.Agency
     {
         static void Main(string[] args)
         {
-            string filePath = "contas.txt";
-            List<Account> listAccount = new List<Account>();
-
-            using (var fileStream = new FileStream(filePath, FileMode.Open))
-            using (var streamReader = new StreamReader(fileStream))
-            {
-                while (!streamReader.EndOfStream)
-                {
-                    string line = streamReader.ReadLine();
-                    //Console.WriteLine(line);
-                    listAccount.Add(ConvertToAccount(line));
-                }
-            }
-            var orderList = listAccount.OrderBy(l => l.Holder.Name);
-
-            foreach (var item in orderList)
-            {
-                Console.WriteLine(item);
-            }
-
-
+           
 
             Console.ReadKey();
-
         }
 
-        static Account ConvertToAccount(string line)
-        {
-            string[] fields = line.Split(' ');
-            int agencyNumber = int.Parse(fields[0]);
-            int AccountNumber = int.Parse(fields[1]);
-            double balance = double.Parse(fields[2]);
-            string name = fields[3];
-
-            return new Account(new Client(name), agencyNumber, AccountNumber, balance);
-
-        }
-
-
+        
     }
 }
---
+++ ByteBank.Agency/Stream/UsingStreamReader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Entities.Employees;
+using ByteBank.Entities;
+using ByteBank.Exceptions;
+using ByteBank.Models;
+using ByteBank.Models.System;
+using Humanizer;
+using System.Text.RegularExpressions;
+using ByteBank.Agency.Extensions;
+using System.IO;
+using System.Globalization;
+
+namespace ByteBank.Agency
+{
+    partial class Program
+    {
+        static void UseStreamReader()
+        {
+            string filePath = "contas.csv";
+
+            List<Account> listAccount = new List<Account>();
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            using (var streamReader = new StreamReader(fileStream))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine();
+                    //Console.WriteLine(line);
+                    listAccount.Add(ConvertToAccount1(line));
+                }
+            }
+            var orderList = listAccount.OrderBy(l => l.Holder.Name);
+
+            foreach (var item in orderList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static Account ConvertToAccount1(string line)
+        {
+            string[] fields = line.Split(',');
+            int agencyNumber = int.Parse(fields[0]);
+            int AccountNumber = int.Parse(fields[1]);
+            double balance = double.Parse(fields[2], CultureInfo.InvariantCulture);
+            string name = fields[3];
+            return new Account(new Client(name), agencyNumber, AccountNumber, balance);
+        }
+    }
+
+
+}
